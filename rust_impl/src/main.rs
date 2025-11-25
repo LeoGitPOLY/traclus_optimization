1abcd6fb4b5473dce3f153f8c247c9ac62d0b252
LeoGitPOLY <leonard.pouliot@polymtl.ca>
2025-11-25 13:33:46 -0500

    Move parse input data to another class

--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -1,54 +1,17 @@
 mod spatial;
 mod utils_io;
 
-use crate::spatial::geometry::Point;
-use crate::spatial::input_od_line::InputODLine;
 use crate::spatial::raw_trajectory_store::Bucket;
 use crate::spatial::raw_trajectory_store::RawTrajectoryStore;
 use crate::spatial::trajectory::Trajectory;
+
+use crate::utils_io::loader::parse_input_data;
 use crate::utils_io::traclus_args::TraclusArgs;
 
 use clap::Parser;
-use std::fs;
 use std::io;
-use std::path::Path;
 
-fn read_file<P: AsRef<Path>>(path: P) -> io::Result<String> {
-    fs::read_to_string(path)
-}
-
-fn parse_od_lines(args: &TraclusArgs) -> RawTrajectoryStore {
-    let content: String = read_file(&args.infile).expect("Failed to read input file");
-    let mut trajectory_storage: RawTrajectoryStore = RawTrajectoryStore::new(args.max_angle);
-
-    for (index, line) in content.lines().enumerate() {
-        let parts: Vec<&str> = line.split_whitespace().collect();
-
-        if parts.len() != 6 {
-            eprintln!("Warning: line {} is malformed: {}", index + 1, line);
-            continue;
-        }
-
-        let od_line: InputODLine = InputODLine {
-            line_id: parts[0].parse().unwrap(),
-            weight: parts[1].parse().unwrap(),
-            start: Point {
-                x: parts[2].parse().unwrap(),
-                y: parts[3].parse().unwrap(),
-            },
-            end: Point {
-                x: parts[4].parse().unwrap(),
-                y: parts[5].parse().unwrap(),
-            },
-        };
-        let trajectory: Trajectory = Trajectory::new(od_line);
-        trajectory_storage.add_trajectory(trajectory);
-    }
-
-    trajectory_storage
-}
-
-// TODO: optimize with maybe the reference of the nearby_trajectories instead of the iterator.collect()
+// TODO: optimize with maybe the reference of the nearby_trajectories vector instead of the iterator.collect()
 fn db_scan_segment_clustering(raw_storage: &RawTrajectoryStore, args: &TraclusArgs) {
     let buckets: &Vec<Bucket> = &raw_storage.traj_buckets;
 
@@ -78,7 +41,7 @@ fn db_scan_segment_clustering(raw_storage: &RawTrajectoryStore, args: &TraclusAr
 }
 fn main() -> io::Result<()> {
     let args: TraclusArgs = TraclusArgs::parse();
-    let raw_storage: RawTrajectoryStore = parse_od_lines(&args);
+    let raw_storage: RawTrajectoryStore = parse_input_data(&args);
 
     db_scan_segment_clustering(&raw_storage, &args);
     raw_storage.print_summary();
--- rust_impl/src/utils_io/mod.rs
+++ rust_impl/src/utils_io/mod.rs
@@ -1,1 +1,2 @@
 pub mod traclus_args;
+pub mod loader;
