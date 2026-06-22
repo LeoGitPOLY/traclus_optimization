155f156240b5d3a235391ea62ea091460113c0ad
LeoGitPOLY <leonard.pouliot@polymtl.ca>
2026-06-22 11:33:57 -0400

    2. Run on alliance-can

--- benchmarks/alliance_can.sh
+++ benchmarks/alliance_can.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=traclus_unit_tests
 #SBATCH --output=traclus_tests_%j.out   # %j = job ID
 #SBATCH --error=traclus_tests_%j.err
-#SBATCH --time=03:00:00                 
+#SBATCH --time=00:30:00                 
 #SBATCH --cpus-per-task=50
 #SBATCH --mem-per-cpu=2G               
 
